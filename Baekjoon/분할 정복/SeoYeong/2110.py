84bae6fb6c53652eb76b9bff8eb9ae7faec4d754
seoyeong98 <kimseozero98@naver.com>
2023-11-08 20:49:31 +0900

    2110 : 공유기설치 - 오류수정, 시간초과 해결

--- Baekjoon/분할 정복/SeoYeong/2110.py
+++ Baekjoon/분할 정복/SeoYeong/2110.py
@@ -41,9 +41,9 @@ def find_max_distance(house: list, C: int):
         mid = (l+r) // 2
         if can_install_router(house, mid, C):
             result = mid
-            left = mid + 1
+            l = mid + 1
         else:
-            right = mid - 1
+            r = mid - 1
     return result
 
 
