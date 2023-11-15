a4b747fc28d00cfbf49e55ae4d5467cad95ec867
yeon0729 <yeon0729@sookmyung.ac.kr>
2023-11-15 22:05:53 +0900

    12015: 가장 긴 증가하는 부분수열 2 - 정답코드 추가

--- Baekjoon/이분 탐색/DyeonPark/12015.py
+++ Baekjoon/이분 탐색/DyeonPark/12015.py
@@ -1,3 +1,43 @@
+# 정답 풀이
+import sys
+
+def input():
+  return sys.stdin.readline().rstrip()
+
+def parametric_search(arr, target):
+  start, end = 0, len(arr) - 1
+
+  while start <= end:
+    mid = (start + end) // 2
+    if arr[mid] >= target:
+      end = mid - 1
+    else:
+      start = mid + 1
+      # result = mid
+
+  return start # 반환값 혹은 반환값 업데이트를 end가 아닌 start에서 했어야 했음
+  # return result  
+
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+LIS = [nums[0]]
+for i in range(1, N):
+  if nums[i] > LIS[-1]: # 선행 원소보다 크거나 같을 때
+    LIS.append(nums[i])
+  else: # 선행 원소보다 작을 때
+    idx = parametric_search(LIS, nums[i])
+    LIS[idx] = nums[i] # 값 대치
+#     print(nums[i], "보다 크지만 가장 작은 수는 LIS의", idx, "에 위치한", LIS[idx])
+
+# print(LIS)
+print(len(LIS))
+
+########################################################
+####################### 틀린 풀이 ########################
+########################################################
+
 import sys
 
 def input():
