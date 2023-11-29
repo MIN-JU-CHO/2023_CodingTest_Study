3887d1820860843dd558d860a4ca760a690fd2b9
seoyeong98 <youngseogimkk@gmail.com>
2023-11-29 20:57:40 +0900

    1012 : 유기농배추 - dfs, runtime error(recursion)

---
+++ Baekjoon/그래프와 순회/SeoYeong/1012_3.py
@@ -0,0 +1,33 @@
+
+def dfs(x: int, y: int):
+    four_direction = [[-1, 0, 1, 0],
+                      [0, -1, 0, 1]]
+    for i in range(4):
+        nx, ny = x + four_direction[0][i], y + four_direction[1][i]
+        if (0 <= nx < n) and (0 <= ny < m) and graph[nx][ny] == 1:
+            graph[nx][ny] = 0
+            dfs(nx, ny)
+    return -1
+
+
+test_case = int(input())
+for _ in range(test_case):
+    m, n, k = map(int, input().split())
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+
+    for _ in range(k):
+        y, x = map(int, input().split())
+        graph[x][y] = 1
+
+    insect = 0
+    if sum(sum(graph, [])) in [0, 1]:
+        print(str(sum(sum(graph, []))))
+        pass
+
+    for x in range(n):
+        for y in range(m):
+            if graph[x][y] == 1:
+                dfs(x, y)
+                insect += 1
+
+    print(insect)
