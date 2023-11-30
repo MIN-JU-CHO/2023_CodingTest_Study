f6fcc84337405a41ab58aa978614b12e9b85dfb4
seoyeong98 <youngseogimkk@gmail.com>
2023-11-30 18:09:01 +0900

    1012 : 유기농배추 - solved

--- Baekjoon/그래프와 순회/SeoYeong/1012_3.py
+++ Baekjoon/그래프와 순회/SeoYeong/1012_3.py
@@ -1,4 +1,5 @@
-
+import sys
+sys.setrecursionlimit(10**9)
 def dfs(x: int, y: int):
     four_direction = [[-1, 0, 1, 0],
                       [0, -1, 0, 1]]
@@ -16,10 +17,10 @@ def dfs_stack(x, y):
     stack = [(x, y)]
     while stack:
         (sx, sy) = stack.pop()
+        graph[sx][sy] = 0
         for i in range(4):
             nx, ny = sx + four_direction[0][i], sy + four_direction[1][i]
             if (0 <= nx < n) and (0 <= ny < m) and graph[nx][ny] == 1:
-                graph[nx][ny] = 0
                 stack.append((nx, ny))
 
 test_case = int(input())
@@ -32,9 +33,6 @@ for _ in range(test_case):
         graph[x][y] = 1
 
     insect = 0
-    if sum(sum(graph, [])) in [0, 1]:
-        print(str(sum(sum(graph, []))))
-        pass
 
     for x in range(n):
         for y in range(m):
