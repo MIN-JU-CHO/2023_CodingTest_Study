1c56491778477ef3ef029b191135c7239f5de081
seoyeong98 <youngseogimkk@gmail.com>
2023-12-13 16:53:56 +0900

    7576. 토마토 시간초과 해결

--- Baekjoon/그래프와 순회/SeoYeong/7576.py
+++ Baekjoon/그래프와 순회/SeoYeong/7576.py
@@ -4,22 +4,30 @@ from collections import deque
 m, n = map(int, input().split())
 warehouse = []
 ripe = deque([])
+numof_unripe = 0
 for i in range(n):
     l = list(map(int, input().split()))
     for j in [j for j, val in enumerate(l) if val == 1]:
         ripe.append((i, j, 0))
+    numof_unripe += len([val for val in l if val == 0])
     warehouse.append(l)
 
 move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visited = set([]) # 초기 노드 넣어주기
 while ripe:
     sx, sy, day  = ripe.popleft()
-    warehouse[sx][sy] = -1
     for dx, dy in move:
         nx, ny = sx + dx, sy + dy
-        if 0<=nx<n and 0<=ny<m and warehouse[nx][ny] == 0:
+        if 0<=nx<n and 0<=ny<m and (nx, ny) not in visited and warehouse[nx][ny] == 0:
             ripe.append((nx, ny, day+1))
+            visited.add((nx, ny))
+            warehouse[nx][ny] = 1
+            numof_unripe -= 1
 
-if sum(sum(warehouse, []))*(-1) == m*n:
+for i in warehouse:
+    print(i)
+
+if numof_unripe == 0:
     print(day)
 else:
     print(-1)
