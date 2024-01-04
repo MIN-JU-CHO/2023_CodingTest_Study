b042bdd36cecad323ee8d482d1db1edd23425d1e
seoyeong98 <youngseogimkk@gmail.com>
2024-01-04 15:52:09 +0900

    2206. 벽부수고 이동하기 - refactor

--- Baekjoon/그래프와 순회/SeoYeong/2206.py
+++ Baekjoon/그래프와 순회/SeoYeong/2206.py
@@ -1,10 +1,23 @@
 from collections import deque
 
+def cardinal_search(next_search_amt: int, break_flag: bool, loop_flag: bool):
+    direction = [(x*next_search_amt, y*next_search_amt) for x, y in cardinal]
+    for dx, dy in direction:
+        nx, ny = sx + dx, sy + dy
+        if 0<=nx<n and 0<=ny<m and (nx, ny) not in visited and graph[nx][ny]==0:
+            visited.add((nx, ny))
+            if next_search_amt == 1: 
+                q.append((nx, ny, move+1, break_flag))
+                loop_flag = False
+            else:
+                q.append((nx, ny, move+1, False))
+    return loop_flag
+
 n, m = map(int, input().split())
 graph = [list(map(int, list(input()))) for _ in range(n)]
 
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-further_direction = [(-2, 0), (0, 2), (2, 0), (0, -2)]
+
+cardinal = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 break_flag = True # 벽 부실 기회 있는지?
 
 visited = set([(0, 0)])
@@ -16,19 +29,10 @@ while q:
         break
 
     loop_flag = True # 4방향 탐색 다 막혀있는지?
-    for dx, dy in direction:
-        nx, ny = sx + dx, sy + dy
-        if 0<=nx<n and 0<=ny<m and (nx, ny) not in visited and graph[nx][ny]==0:
-            visited.add((nx, ny))
-            q.append((nx, ny, move+1, break_flag))
-            loop_flag = False
+    loop_flag = cardinal_search(1, break_flag, loop_flag)
 
     if loop_flag and break_flag:
-        for dx, dy in further_direction:
-            nx, ny = sx + dx, sy + dy
-            if 0<=nx<n and 0<=ny<m and (nx, ny) not in visited and graph[nx][ny]==0:
-                visited.add((nx, ny))
-                q.append((nx, ny, move+1, False))
+        cardinal_search(2, break_flag, loop_flag)
 
 
 print(move+1)
