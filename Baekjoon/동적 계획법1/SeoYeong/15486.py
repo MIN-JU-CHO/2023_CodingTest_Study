2bb7d7f5e5a31884b518a047ca79cf089616a836
seoyeong98 <youngseogimkk@gmail.com>
2024-01-15 21:47:05 +0900

    [dp] 15486. 퇴사2 - timeout

---
+++ Baekjoon/동적 계획법1/SeoYeong/15486.py
@@ -0,0 +1,19 @@
+n = int(input())
+time, profit = [], []
+for _ in range(n):
+    t, p = map(int, input().split())
+    time.append(t)
+    profit.append(p)
+
+dp = [0 for _ in range(n+1)]
+
+for i in range(n-1, -1, -1):
+    if i + time[i] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], profit[i] + dp[i+time[i]])
+    print(dp)
+
+
+
+print(f"answer is {max(dp)}")
