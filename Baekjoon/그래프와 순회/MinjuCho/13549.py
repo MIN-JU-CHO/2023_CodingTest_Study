1676fdd18cde9249468e6eda8a1991fe98af413b
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2024-01-17 18:17:26 +0900

    숨바꼭질 3 13549 - 파이썬

---
+++ Baekjoon/그래프와 순회/MinjuCho/13549.py
@@ -0,0 +1,29 @@
+from queue import deque
+def teleport(cur, sec):
+    while cur<=100000 and visited[cur] == -1:
+        q.append(cur)
+        visited[cur] = sec
+        if cur == K:
+            print(visited[cur])
+            exit()
+        cur *= 2
+        
+N, K = map(int, input().strip().split())
+visited = [-1 for _ in range(100002)]
+d = [-1, 1]
+q = deque()
+
+q.append(N)
+visited[N] = 0
+teleport(N*2, 0)
+while q:
+    cur = q.popleft()
+    for offset in d:
+        if cur+offset < 0 or cur+offset >=100002:
+            continue
+        if visited[cur+offset] != -1:
+            continue
+        q.append(cur+offset)
+        visited[cur+offset] = visited[cur] + 1
+        teleport((cur+offset)*2, visited[cur+offset])
+print(visited[K])
